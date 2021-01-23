ed3273d4b95579a03bce65d5dcc3c2937cf97fba
Matteo Makovec <matteo.makovec@gmail.com>
2021-01-23 15:28:20 +0100

    Add files via upload

--- gamified_marketingTABLECREATION.sql
+++ gamified_marketingTABLECREATION.sql
@@ -12,7 +12,8 @@ CREATE TABLE Usertable(
 	username varchar(25) NOT NULL,
 	passwd varchar(50) NOT NULL,
 	email varchar(50) DEFAULT NULL,
-	last_login varchar(10) DEFAULT NULL
+	last_login varchar(20) DEFAULT NULL,
+    user_type varchar(6) DEFAULT 'Normal' CHECK (user_type IN ('Normal', 'Admin'))
 );
 
 INSERT INTO Usertable (ID, username, passwd, email)
@@ -21,17 +22,17 @@ VALUES (0001, 'user1', 'user1', 'user1@gmail.com');
 INSERT INTO Usertable (ID, username, passwd, email)
 VALUES (0002, 'user2', 'user2', 'user2@gmail.com');
 
+INSERT INTO Usertable (ID, username, passwd, email, user_type)
+VALUES (0003, 'user3', 'user3', 'user3@gmail.com', 'Admin');
+
 
 -- ------------------------------------------------------------------------ --
 -- Product -- 
 CREATE TABLE Product(
 	ID_product int PRIMARY KEY auto_increment,
 	p_name varchar(75) NOT NULL,
-	image BLOB
+	image MEDIUMBLOB
 );
-
-INSERT INTO Product (ID_product, p_name, image)
-VALUES (1010, 'CLup', null);
 
 
 -- ------------------------------------------------------------------------ --
@@ -40,25 +41,8 @@ CREATE TABLE Questionnaire(
 	ID_questionnaire int PRIMARY KEY auto_increment,
     q_date varchar(10) NOT NULL,
     product_idx int,
-    FOREIGN KEY (product_idx) REFERENCES Product (ID_product) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (product_idx) REFERENCES Product (ID_product)
 );
-
-INSERT INTO Questionnaire (ID_questionnaire, q_date, product_idx)
-VALUES (1111, '2021-01-06', 1010);
-
-
--- ------------------------------------------------------------------------ --
--- Submit -- 
-CREATE TABLE Submit(
-	user_idx int,
-    questionnaire_idx int,
-    PRIMARY KEY (user_idx, questionnaire_idx),
-    FOREIGN KEY (user_idx) REFERENCES Usertable (ID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (questionnaire_idx) REFERENCES Questionnaire (ID_questionnaire) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-INSERT INTO Submit (user_idx, questionnaire_idx)
-VALUES (0001, 1111);
 
 
 -- ------------------------------------------------------------------------ --
@@ -68,56 +52,20 @@ CREATE TABLE Question(
     question varchar(200) NOT NULL,
     q_type varchar(11) CHECK (q_type IN ('Statistical', 'Marketing')) NOT NULL,
     questionnaire_idx int,
-    FOREIGN KEY (questionnaire_idx) REFERENCES Questionnaire (ID_questionnaire) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (questionnaire_idx) REFERENCES Questionnaire (ID_questionnaire) ON UPDATE CASCADE
 );
-
-INSERT INTO Question (ID_question, question, q_type, questionnaire_idx)
-VALUES (0001, 'Marketing Question 1', 'Marketing', 1111);
-
-INSERT INTO Question (ID_question, question, q_type, questionnaire_idx)
-VALUES (0002, 'Marketing Question 2', 'Marketing', 1111);
-
-INSERT INTO Question (ID_question, question, q_type, questionnaire_idx)
-VALUES (0003, 'Marketing Question 3', 'Marketing', 1111);
-
-INSERT INTO Question (ID_question, question, q_type, questionnaire_idx)
-VALUES (0004, 'Statistical Question 1', 'Statistical', 1111);
-
-INSERT INTO Question (ID_question, question, q_type, questionnaire_idx)
-VALUES (0005, 'Statistical Question 2', 'Statistical', 1111);
-
-INSERT INTO Question (ID_question, question, q_type, questionnaire_idx)
-VALUES (0006, 'Statistical Question 3', 'Statistical', 1111);
 
 
 -- ------------------------------------------------------------------------ --
 -- Answer -- 
 CREATE TABLE Answer(
 	ID_answer int PRIMARY KEY auto_increment,
-	answer varchar(200),
-	user_idx int,
-    question_idx int,
-    FOREIGN KEY (user_idx) REFERENCES Usertable(ID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (question_idx) REFERENCES Question(ID_question) ON DELETE CASCADE ON UPDATE CASCADE
+	answer varchar(250) NOT NULL,
+	user_idx int NOT NULL,
+    question_idx int NOT NULL,
+    FOREIGN KEY (user_idx) REFERENCES Usertable (ID),
+    FOREIGN KEY (question_idx) REFERENCES Question (ID_question)
 );
-
-INSERT INTO Answer (ID_answer, answer, user_idx, question_idx)
-VALUES (0001, 'Answer 1', 0001, 0001);
-
-INSERT INTO Answer (ID_answer, answer, user_idx, question_idx)
-VALUES (0002, 'Answer 2', 0001, 0002);
-
-INSERT INTO Answer (ID_answer, answer, user_idx, question_idx)
-VALUES (0003, 'Answer 3', 0001, 0003);
-
-INSERT INTO Answer (ID_answer, answer, user_idx, question_idx)
-VALUES (0004, 'Answer 4', 0001, 0004);
-
-INSERT INTO Answer (ID_answer, answer, user_idx, question_idx)
-VALUES (0005, 'Answer 5', 0001, 0005);
-
-INSERT INTO Answer (ID_answer, answer, user_idx, question_idx)
-VALUES (0006, 'Answer 6', 0001, 0006);
 
 
 -- ------------------------------------------------------------------------ --
@@ -134,3 +82,31 @@ VALUES ('badword2');
 
 INSERT INTO BadWord (word)
 VALUES ('badword3');
+
+
+-- ------------------------------------------------------------------------ --
+-- Leaderboard -- 
+CREATE TABLE Leaderboard(
+	ID int,
+	questionnaire int,
+    points int,
+    PRIMARY KEY (ID, questionnaire),
+    FOREIGN KEY (ID) REFERENCES Usertable (ID),
+    FOREIGN KEY (questionnaire) REFERENCES Questionnaire (ID_questionnaire)
+);
+
+
+-- ------------------------------------------------------------------------ --
+-- ------------------ TRIGGERS ------------------ -- 
+DELIMITER $$
+
+CREATE TRIGGER CopyStatisticalQuestion
+AFTER INSERT ON `Questionnaire`
+FOR EACH ROW
+BEGIN
+    INSERT INTO Question (question, q_type, questionnaire_idx) VALUES ('Age', 'Statistical', new.ID_questionnaire);
+    INSERT INTO Question (question, q_type, questionnaire_idx) VALUES ('Sex', 'Statistical', new.ID_questionnaire);
+	INSERT INTO Question (question, q_type, questionnaire_idx) VALUES ('Expertise level', 'Statistical', new.ID_questionnaire);
+END$$
+
+DELIMITER ;
